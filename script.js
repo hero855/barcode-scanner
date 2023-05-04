217e9adc41805b1da305731e409b850367d95f41
ilya.ozerov <ilya.ozerov@joy-dev.com>
2023-05-04 04:43:17 +0300

    Added barcode scanner

---
+++ script.js
@@ -0,0 +1,15 @@
+function onScanSuccess(decodedText, decodedResult) {
+    // handle the scanned code
+    console.log(`Code matched = ${decodedText}`, decodedResult);
+}
+
+function onScanFailure(error) {
+    // handle scan failure
+    console.warn(`Code scan error = ${error}`);
+}
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader",
+    { fps: 10, qrbox: { width: 250, height: 250 } },
+    /* verbose= */ false);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
