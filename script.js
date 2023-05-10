312acbf31b8df16419bdce3950a2d75d36ca985c
ilya.ozerov <ilya.ozerov@joy-dev.com>
2023-05-10 03:56:48 +0300

    Activated telegram sendData call on scan success

--- script.js
+++ script.js
@@ -4,12 +4,7 @@ var resultContainer = document.getElementById('qr-reader-results');
 var lastResult, countResults = 0;
 
 function onScanSuccess(decodedText, decodedResult) {
-    if (decodedText !== lastResult) {
-        ++countResults;
-        lastResult = decodedText;
-        // Handle on success condition with the decoded message.
-        console.log(`Scan result ${decodedText}`, decodedResult);
-    }
+    window.Telegram.WebApp.sendData({ decodedText, decodedResult });
 }
 
 var html5QrcodeScanner = new Html5QrcodeScanner(
